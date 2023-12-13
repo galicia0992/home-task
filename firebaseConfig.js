59a9b8c6a02c7b530ccdaded449a0cf6eb2963df
galicia0992 <perrako92@live.com.mx>
2023-12-13 15:06:52 -0600

    error auth solucionado

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,6 +4,8 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import {getDatabase} from "firebase/database"
 import { getFirestore, getDocs, collection } from "firebase/firestore";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,9 +23,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const FIREBASE_APP = initializeApp(firebaseConfig);
+initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
 export const db = getFirestore(FIREBASE_APP)
 export const database = getDatabase(FIREBASE_APP)
