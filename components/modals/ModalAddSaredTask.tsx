984e8aa431d7487de8aa4e10d942309e22f848fa
galicia0992 <perrako92@live.com.mx>
2024-02-11 16:49:11 -0600

    select options

--- components/modals/ModalAddSaredTask.tsx
+++ components/modals/ModalAddSaredTask.tsx
@@ -1,32 +1,123 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Button, Card, Modal, Text } from '@ui-kitten/components';
 import { useBooleanModalAddContext } from '../../context/ModalAddContext';
 import { useSetBooleanModalAddContext } from '../../context/ModalAddContext';
+import {
+  Select,
+  SelectTrigger,
+  SelectInput,
+  SelectIcon,
+  SelectPortal,
+  Icon,
+  SelectBackdrop,
+  SelectItem,
+  SelectContent,
+  ChevronDownIcon,
+  SelectDragIndicator,
+  SelectDragIndicatorWrapper,
+  Center,
+  Modal,
+  ModalBackdrop,
+  ModalContent,
+  Heading,
+  ModalCloseButton,
+  CloseIcon,
+  ModalBody,
+  ModalHeader,
+  Text,
+  ModalFooter,
+  Button,
+  ButtonText,
+} from '@gluestack-ui/themed';
 
 export const ModalAddSharedTask = (): React.ReactElement => {
 const setBooleanModalAdd = useSetBooleanModalAddContext()
 const booleanModalAdd = useBooleanModalAddContext()
+const [selected, setSelected] = useState<string>('');
+  const ref = React.useRef(null);
+
+  useEffect(() => {
+    console.log(selected);
+  }, [selected]);
 
   return (
-    <View style={styles.container}>
-
+    <Center h={300}>
       <Modal
-        visible={booleanModalAdd}
-        backdropStyle={styles.backdrop}
-        onBackdropPress={() => setBooleanModalAdd(false)}
-      >
-        <Card disabled={true}>
-          <Text>
-Welcome to UI Kitten Shared 😻
-          </Text>
-          <Button onPress={() => setBooleanModalAdd(false)}>
-            DISMISS
-          </Button>
-        </Card>
+        isOpen={booleanModalAdd}
+        onClose={() => {
+          setBooleanModalAdd(false);
+        }}
+        finalFocusRef={ref}>
+        <ModalBackdrop />
+        <ModalContent>
+          <ModalHeader>
+            <Heading size="lg">Engage with Modals</Heading>
+            <ModalCloseButton>
+              <Icon as={CloseIcon} />
+            </ModalCloseButton>
+          </ModalHeader>
+          <ModalBody>
+            <Text>
+              Elevate user interactions with our versatile modals. Seamlessly
+              integrate notifications, forms, and media displays. Make an impact
+              effortlessly.
+            </Text>
+            <Select
+              onValueChange={value => setSelected(value)}
+              style={{zIndex: 2}}>
+              <SelectTrigger variant="outline" size="md">
+                <SelectInput placeholder="Select option" />
+                <SelectIcon mr="$3">
+                  <Icon as={ChevronDownIcon} />
+                </SelectIcon>
+              </SelectTrigger>
+              <SelectPortal>
+                <SelectBackdrop />
+                <SelectContent>
+                  <SelectDragIndicatorWrapper>
+                    <SelectDragIndicator />
+                  </SelectDragIndicatorWrapper>
+                  <SelectItem label="UX Research" value="ux" />
+                  <SelectItem label="Web Development" value="web" />
+                  <SelectItem
+                    label="Cross Platform Development Process"
+                    value="Cross Platform Development Process"
+                  />
+                  <SelectItem
+                    label="UI Designing"
+                    value="ui"
+                    isDisabled={true}
+                  />
+                  <SelectItem label="Backend Development" value="backend" />
+                </SelectContent>
+              </SelectPortal>
+            </Select>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              variant="outline"
+              size="sm"
+              action="secondary"
+              mr="$3"
+              onPress={() => {
+                setBooleanModalAdd(false);
+              }}>
+              <ButtonText>Cancel</ButtonText>
+            </Button>
+            <Button
+              size="sm"
+              action="positive"
+              borderWidth="$0"
+              onPress={() => {
+                setBooleanModalAdd(false);
+              }}>
+              <ButtonText>Explore</ButtonText>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
       </Modal>
-
-    </View>
+    </Center>
   );
 };
 
